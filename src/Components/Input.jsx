8241393f0a82759740637d6b4edd304c039d5b8d
0xNeolik <alvaro.teran@ontheuser.com>
2022-08-26 12:52:51 +0200

    Code for Input.jsx

--- src/Components/Input.jsx
+++ src/Components/Input.jsx
@@ -1,14 +1,36 @@
-import React from 'react'
-import PropTypes from "prop-types"
+import React from "react";
+import PropTypes from "prop-types";
 
-function Input({secretWord}) {
+function Input({ secretWord }) {
+  const [guessedWord, setGuessedWord] = React.useState("");
+
+  const clickButton = (event) => {
+    event.preventDefault;
+  };
+
   return (
-    <div data-test="component-input">Input</div>
-  )
+    <div data-test="component-input">
+      <form className="form-inline">
+        <input
+          data-test="input-box"
+          className="mb-2 mx-sm-3"
+          type="text"
+          placeholder="Enter Guess"
+        />
+        <button
+          className="btn btn-primary mb-2"
+          data-test="submit-button"
+          onclick={clickButton}
+        >
+          Submit
+        </button>
+      </form>
+    </div>
+  );
 }
 
 Input.propTypes = {
-    secretWord: PropTypes.string.isRequired
-}
+  secretWord: PropTypes.string.isRequired,
+};
 
-export default Input
+export default Input;
